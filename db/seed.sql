1e430365860a9b2d3c2d7e3d78bdfe4da38b5cfc
Thomas Whittle <tewhittle@gmail.com>
2021-01-25 17:32:31 -0500

    Begin to add basic infrastructure, including npm packages and database files

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured)
+    VALUES ("Whopper", false),
+    VALUES ("Big Mac", false),
+    VALUES ("Arch Deluxe", false),
+    VALUES ("Jr Bacon Cheeseburger", false);
