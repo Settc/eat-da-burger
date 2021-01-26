4f6e744012317e10d6b03e8731582db267c8809b
Thomas Whittle <tewhittle@gmail.com>
2021-01-26 14:17:04 -0500

    Add insert and update functionality to orm

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,29 @@
 const connection = require("./connection.js")
 
+function questionMarks(num) {
+    let arr = []
+    for (let i = 0; i < num; i++) {
+        arr.push("?")
+    }
+
+    return arr.toString()
+}
+
+function objToSql(ob) {
+    let arr = []
+    for (let key in ob) {
+        let value = ob[key]
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'"
+            }
+            arr.push(key + "=" + value)
+        }
+    }
+
+    return arr.toString()
+}
+
 const orm = 
 {
     selectAll: function(tableInput, cb) {
@@ -8,8 +32,27 @@ const orm =
             if (err) throw err
             cb(result)
         })
-    }
-    
+    },
+    createOne: function(table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + questionMarks(vals.length) + ") "
+        console.log(queryString)
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) throw err
+
+            cb(result)
+        })
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table + "SET " + objToSql(objColVals) + " WHERE " + condition
+        console.log(queryString)
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) throw err
+
+            cb(result)
+        })
+    }        
 }
 
 module.exports = orm
