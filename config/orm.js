697deee91f19360b450d49aebababa8a3d569bc4
Thomas Whittle <tewhittle@gmail.com>
2021-01-26 13:47:03 -0500

    Add selectAll function to orm

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,16 @@
 const connection = require("./connection.js")
 
+const orm = 
+{
+    selectAll: function() 
+        {
+            const queryString = "SELECT * FROM burgers"
+            connection.query(queryString, function(err, res) 
+            {
+                if (err) throw err
+                console.log(result)
+            })
+        }
+}
+
+module.exports = orm
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const express = require("./express")
+const express = require("express")
+const exphbs = require("express-handlebars")
+const app = express()
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() 
+{
+    console.log("Server listening on: http://localhost:" + PORT)
+})
